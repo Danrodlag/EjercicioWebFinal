03b6d866fa0b70c61a3c44ea2727ace012a404b3
Daniel <drodenaslagares@gmail.com>
2022-11-24 09:47:52 +0100

    Ranking hecho, carrusel arreglado

---
+++ scripts/rankscript.js
@@ -0,0 +1,25 @@
+$(function () {
+    "use strict";
+    $("#tablarank").jqGrid({
+        colModel: [
+            { name: "Intérprete" },
+            { name: "Canción" },
+            { name: "NºDescargas"},
+            { name: "Precio"},
+            { name: "Versionado", template: "booleanCheckbox"},
+            { name: "ÚltDescarga"} 
+        ],
+        data: [
+            { id: 10, Intérprete: "Joan Manuel Serrat", Canción: "Mediterráneo", NºDescargas: "1334987", Precio: "1.25", Versionado: false, ÚltDescarga: "02-Sep-2021"  },
+            { id: 20, Intérprete: "Natalia Dicenta", Canción: "Summertime", NºDescargas: "827994", Precio: "1.05", Versionado: true, ÚltDescarga: "12-Oct-2021" },
+            { id: 30, Intérprete: "Andrea Mois Quartet", Canción: "He's funny that way", NºDescargas: "672910", Precio: "0.80", Versionado: true, ÚltDescarga: "28-Jul-2021" },
+            { id: 40, Intérprete: "Los Ronaldos", Canción: "Adiós Papá", NºDescargas: "513845", Precio: "1.00", Versionado: false, ÚltDescarga: "5-Aug-2021" },
+            { id: 50, Intérprete: "Pablo Alborán", Canción: "Inséparables", NºDescargas: "421990", Precio: "0.90", Versionado: false, ÚltDescarga: "14-May-2021"  },
+            { id: 60, Intérprete: "Rozalén", Canción: "80 veces", NºDescargas: "355682", Precio: "0.95", Versionado: false, ÚltDescarga: "30-Aug-2021"  },
+            { id: 70, Intérprete: "Pedro Guerra", Canción: "Niños", NºDescargas: "202347", Precio: "1.15", Versionado: false, ÚltDescarga: "23-Sep-2021"  },
+            { id: 80, Intérprete: "Rosalía", Canción: "Me quedo contigo", NºDescargas: "117906", Precio: "1.20", Versionado: true, ÚltDescarga: "31-Jul-2021"  }
+        ],
+        pager: true ,
+        rowNum: 5,
+    });
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,55 @@
-
+/*Carrusel de imágenes, donde la primera es aleatoria */
+/* Iniciamos la lista con las rutas a las imágenes */
 var IMAGENES = ['img/inicio//carrusel/1.png','img/inicio//carrusel/2.png','img/inicio//carrusel/3.png'];
-var posicionActual = 0;
+
+/* Creamos la variable con la posición de la lista que pasaremos, de forma aleatoria */
+var posicionActual = Math.floor(Math.random() * 2);
+
+/* Pasamos la primera imagen aleatoria */
 var intervalo;
+document.getElementById("tododiscos").src = IMAGENES[posicionActual]
+
+/* Creamos la función pasar foto, que irá pasando por todas las imágenes */
 function pasar_foto() {
     if(posicionActual >= IMAGENES.length - 1) {
         posicionActual = 0;
     } else {
         posicionActual++;
     }
-    console.log(posicionActual);
-    document.getElementById("tododiscos").src = IMAGENES[posicionActual];
-    
+    document.getElementById("tododiscos").src = IMAGENES[posicionActual];  
 };
-console.log("Funciono");
+
+/* Creamos la variable del intervalo de cambio que pasaremos */
 const TIEMPO_INTERVALO_MILESIMAS_SEG = 5000;
+/* Usamos la función setInterval para cada 5 segundos ejecutar la función */
 intervalo = setInterval(pasar_foto, TIEMPO_INTERVALO_MILESIMAS_SEG);
 
+
+/* Para lo próximo, crearemos la función pedirenlace, que se ejecutará desde pediremos enlaces, que le da un retraso */
 function pedirenlace(){
+
+    /* Cogemos todos los enlaces del html */
     var enlaces = document.getElementsByTagName("a")
+
+    /* Preguntamos cuantos enlaces cree el usuario que tiene la web */
     var pregunta = window.prompt("¿Cuántos enlaces crees que tiene esta página?")
+
+    /* Si acierta */
     if (pregunta == enlaces.length){
+        /* Mandar un mensaje de que está bien */
         window.alert(`Muy bien, has acertado tiene ${enlaces.length} enlaces`);
+
+    /* Si le da a cancelar, que mande otro mensaje */
     } else if (pregunta == null){
         window.alert("Vale, ya lo pillo. No te apetece contestar... Otra vez será...")
+    
+    /* Y si falla, decirle cuantos hay */
     } else {
         window.alert(`Ups... me temo que te has equivocado hay ${enlaces.length} enlaces. Más suerte la próxima vez...`)
     }
 };
 
+/* Esta es la función que le da el retraso */
 function pediremosnlaces(){
     setTimeout(pedirenlace, 15000);
 };
-
-
